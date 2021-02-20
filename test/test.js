42ddc2a3846209e33b18a9e485eada0a84efaeda
Pavel Maslov <pavel.maslov@tretton37.com>
2021-02-20 14:00:59 +0100

    moved to test folder

--- test/test.js
+++ test/test.js
@@ -1,4 +1,4 @@
-const { encode, decode } = require('./lib/upnqr')
+const { encode, decode } = require('../lib/upnqr')
 const assert = require('assert')
 const describe = require('mocha').describe
 const it = require('mocha').it
